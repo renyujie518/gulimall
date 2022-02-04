f8399e42df3ccf9eb22cdf9c1dad728d8102a0d0
renyujie518 <903356741@qq.com>
2022-02-04 20:34:23 +0800

    去除RAM子账号 防泄漏

--- gulimall-product/src/test/java/com/renyujie/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/renyujie/gulimall/product/GulimallProductApplicationTests.java
@@ -51,8 +51,8 @@ class GulimallProductApplicationTests {
        // // Endpoint以杭州为例，其它Region请按实际情况填写。
        // String endpoint = "http://oss-cn-beijing.aliyuncs.com";
        //// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-       // String accessKeyId = "LTAI5t8tQGAjN4MpNrfv2k2Q";
-       // String accessKeySecret = "66p27jI71YB6KmZG3b48jALPh0o6Dy";
+       // String accessKeyId = "";
+       // String accessKeySecret = "";
 	   //
        //// 创建OSSClient实例。
        // OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
